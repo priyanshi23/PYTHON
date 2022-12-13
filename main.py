c60697fe2bc49eb6609865b033e07d90acaba4b6
Priyanshi Tripathi <priyanshi23smc@gmail.com>
2022-12-13 20:29:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,44 +1,108 @@
-# This is a sample Python script.
+from tkinter import *
+root = Tk()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+root.title("Calculator")
 
-# noinspection PyListCreation
-timeTable = []
-#Monday
-timeTable.append(["History", "Maths", "PE", "Computer Science", "Music"])
+e = Entry(root, width=35,borderwidth=5)
+e.grid(row=0,column=0,columnspan=3,padx=10,pady=10)
 
-#Tuesday
-timeTable.append(["English", "Maths", "Spanish", "Geography", "Art"])
+def button_click(number):
+   current =e.get()
+   e.delete(0,END)
+   e.insert(0,str(current)+str(number))
 
-#Wednesday
-timeTable.append(["PE", "English", "Science", "Art", "PE"])
+def button_clear():
+    e.delete(0, END)
 
-#Thursday
-timeTable.append(["Maths", "English", "Philosophy", "Spanish", "Music"])
+def button_add():
+  first_number = e.get()
+  global f_num
+  global maths
+  maths = "addition"
+  f_num = int(first_number)
+  e.delete(0, END)
 
-#Friday
-timeTable.append(["Science", "Drama", "History", "Geography", "Science"])
+def button_equal():
+    second_number = e.get()
+    e.delete(0,END)
 
-day = input("Day of the week?").title()
-period = int(input("Lesson Number(1to5):"))
-while period < 1 or period > 5:
-    period = int(input("Lesson Number(1to5):"))
-    lesson = ""
-    if day == "Monday":
-        lesson = timeTable[0][period-1]
-    elif day == "Tuesday":
-        lesson = timeTable[1][period-1]
-    elif day == "Wednesday":
-        lesson = timeTable[2][period-1]
-    elif day == "Thursday":
-        lesson = timeTable[3][period-1]
-    elif day == "Friday":
-        lesson = timeTable[4][period-1]
-    else:
-        print("Not a valid week day!")
-        lesson = timeTable[2][period-1]
+    if maths == "addition":
+       e.insert(0, f_num + int(second_number))
+    if maths == "subtraction":
+       e.insert(0, f_num - int(second_number))
+    if maths == "multiplication":
+       e.insert(0, f_num * int(second_number))
+    if maths == "division":
+       e.insert(0, f_num / int(second_number))
 
-        if lesson != "":
-            print("On" + day + ",lesson " + str(period) + " you have " + lesson + " .")
+def button_subtract():
+   first_number = e.get()
+   global f_num
+   global maths
+   maths = "subtraction"
+   f_num = int(first_number)
+   e.delete(0, END)
+
+def button_multiply():
+   first_number = e.get()
+   global f_num
+   global maths
+   maths = "multiplication"
+   f_num = int(first_number)
+   e.delete(0, END)
+
+def button_divide():
+   first_number = e.get()
+   global f_num
+   global maths
+   maths = "division"
+   f_num = int(first_number)
+   e.delete(0, END)
+
+
+#define buttons
+button_1 = Button(root,text="1",padx=40,pady=20,command=lambda :button_click(1))
+button_2 = Button(root,text="2",padx=40,pady=20,command=lambda :button_click(2))
+button_3 = Button(root,text="3",padx=40,pady=20,command=lambda :button_click(3))
+button_4 = Button(root,text="4",padx=40,pady=20,command=lambda :button_click(4))
+button_5 = Button(root,text="5",padx=40,pady=20,command=lambda :button_click(5))
+button_6 = Button(root,text="6",padx=40,pady=20,command=lambda :button_click(6))
+button_7 = Button(root,text="7",padx=40,pady=20,command=lambda :button_click(7))
+button_8 = Button(root,text="8",padx=40,pady=20,command=lambda :button_click(8))
+button_9 = Button(root,text="9",padx=40,pady=20,command=lambda :button_click(9))
+button_0 = Button(root,text="0",padx=40,pady=20,command=lambda :button_click(0))
+button_add = Button(root,text="+",padx=39,pady=20,command=button_add)
+button_equal = Button(root,text="=",padx=91,pady=20,command=button_equal)
+button_clear = Button(root,text="Clear",padx=79,pady=20,command=button_clear)
+button_subtract = Button(root,text="-",padx=41,pady=20,command=button_subtract)
+button_multiply = Button(root,text="*",padx=40,pady=20,command=button_multiply)
+button_divide = Button(root,text="/",padx=41,pady=20,command=button_divide)
+
+
+#Put the buttons on the screen
+button_1.grid(row=3,column=0)
+button_2.grid(row=3,column=1)
+button_3.grid(row=3,column=2)
+
+button_4.grid(row=2,column=0)
+button_5.grid(row=2,column=1)
+button_6.grid(row=2,column=2)
+
+button_7.grid(row=1,column=0)
+button_8.grid(row=1,column=1)
+button_9.grid(row=1,column=2)
+
+button_0.grid(row=4,column=0)
+button_add.grid(row=5,column=0)
+button_equal.grid(row=5,column=1,columnspan=2)
+button_clear.grid(row=4,column=1,columnspan=2)
+
+button_subtract.grid(row=6,column=0,)
+button_multiply.grid(row=6,column=1)
+button_divide.grid(row=6,column=2)
+
+
+root.mainloop()
+
+
+
