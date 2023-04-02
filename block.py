58d6471162d0b0b72fd7834c4aa4049c94f1ab76
Priyanshi Tripathi <priyanshi23smc@gmail.com>
2023-04-02 16:44:27 +0200

    Create block.py

---
+++ block.py
@@ -0,0 +1,11 @@
+#pRIYANSHI TRIPATHI
+# I am stubborn
+initial = """ PPPP
+              P   P
+              P   P
+              PPPP
+              P
+              P
+              P 
+             """
+print(initial)
